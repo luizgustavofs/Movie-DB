3378bd91be30661f59e08111181c705b63f41651
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-02-22 17:06:27 -0300

    🚧 Create App

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons'; 
+
+import Movies from './src/Screens/Movies';
+import Series from './src/Screens/Series';
+import Search from './src/Screens/Search';
+import Soon from './src/Screens/Soon';
+import { StatusBar } from 'react-native';
+import { AnimatedTabBarNavigator } from 'react-native-animated-nav-tab-bar';
+import { Calendar, Movie, SearchIcon, TvShow } from './src/svgs';
+
+
+const Tab = AnimatedTabBarNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NavigationContainer>
+    <StatusBar barStyle={'light-content'}/>
+    <Tab.Navigator 
+    // screenOptions={{headerShown: false, tabBarStyle: {backgroundColor: '#1B1721', borderTopColor: 'transparent'}}}
+    tabBarOptions={{
+      activeTintColor: "#fff",
+      inactiveTintColor: "#000",
+      labelStyle:{
+        fontSize: 14,
+      },
+      activeBackgroundColor: '#DE1A3A'
+    }}
+    appearance={{tabBarBackground: '#1B1721'}}
+  >
+
+      <Tab.Screen 
+      name="Series" 
+      component={Series}
+      options={{
+        tabBarIcon: ({ focused, color, size }) => (
+            <TvShow
+              height={size ? size : 24}
+              width={size ? size : 24}
+              color={focused ? color : "#B8B7C3"}
+            />
+        )
+      }}
+      
+      />
+      <Tab.Screen 
+      name="Movies" 
+      component={Movies}
+      options={{
+        tabBarIcon: ({ focused, color, size }) => (
+            <Movie
+                height={size ? size : 24}
+                width={size ? size : 24}
+                color={focused ? color : "#B8B7C3"}
+            />
+        )
+      }}
+      
+      />
+      <Tab.Screen 
+      name="Search" 
+      component={Search}
+      options={{
+        tabBarIcon: ({ focused, color, size }) => (
+            <SearchIcon
+              height={size ? size : 24}
+              width={size ? size : 24}
+              color={focused ? color : "#B8B7C3"}
+            />
+        )
+      }}
+      
+      />
+      <Tab.Screen 
+      name="Calendar" 
+      component={Soon}
+      options={{
+        tabBarIcon: ({ focused, color, size }) => (
+            <Calendar
+              height={size ? size : 24}
+              width={size ? size : 24}
+              color={focused ? color : "#B8B7C3"}
+            />
+        )
+      }}
+      
+      />
+
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
