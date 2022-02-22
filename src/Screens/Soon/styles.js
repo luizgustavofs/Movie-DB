ec9ca5e7399b03a88f77c63bc60bfb2695af67f8
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-02-22 17:39:52 -0300

    💄 Create styled componentes to Screens Soon

---
+++ src/Screens/Soon/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #1B1721;
+
+`;
+
+export const Title = styled.Text`
+  margin-top: 80px;
+  margin-bottom: 20px;
+
+  font-size: 28px;
+  font-weight: bold;
+
+  color: #fff;
+`;
+
+export const Movies = styled.ScrollView``;
+
+export const MoviesView = styled.View`
+  height: 350px;
+  width: 100%;
+`;
+
+export const Space = styled.View`
+  height: 10px;
+  width: 100%;
+
+  background: #DD1B3A;
+`;
