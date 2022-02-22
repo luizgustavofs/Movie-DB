147d91c455d2525a7eaf919f08844439fdaa900d
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-02-22 17:41:45 -0300

    ✨ Create Screens Search

---
+++ src/Screens/Search/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import SearchMovie from '../../../components/SearchMovie';
+
+// import { Container } from './styles';
+
+const Search = () => {
+  return (
+    <SearchMovie />
+  );
+}
+
+export default Search;
